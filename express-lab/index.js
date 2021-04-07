fe4cb5ed89bd6f04cfc8acfc17d512217a149660
Orange-J <juhaotian@foxmail.com>
2021-04-07 20:20:34 +0800

    express添加静态文件处理

--- express-lab/index.js
+++ express-lab/index.js
@@ -13,6 +13,13 @@ app.get('/', (req, res) => {
   // res.end();
 });
 
+// 静态资源
+app.use('/static', express.static('public', {
+  setHeaders: (res) => {
+    res.set('access-control-allow-origin', '*');
+  }
+}));
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
