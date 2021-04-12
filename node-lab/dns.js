c73fe665848608fcd3151c68428d5b0c37e73e7d
Orange-J <juhaotian@foxmail.com>
2021-04-12 20:33:11 +0800

    修改dns为all模式

--- node-lab/dns.js
+++ node-lab/dns.js
@@ -1,9 +1,14 @@
 const dns = require('dns');
 
-dns.lookup('www.baidu.com', (err, address, family) => {
+dns.lookup('www.baidu.com', {
+  all: true,
+}, (err, addressList) => {
   if (err) {
     console.log('DNS解析出错:', err);
     return;
   }
-  console.log(`address: ${address}, family: ${family}`);
+
+  for (const { address, family } of addressList) {
+    console.log(`addr: ${address}, family: ${family}`);
+  }
 });
